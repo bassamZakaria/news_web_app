2dd94146887a4c34244bb75367bf22dacf68a9ce
Bassam Salah Zakaria <fbsam.tkrcl@gmail.com>
2020-02-12 21:57:32 +0200

    Add news modal

--- src/lib/components/News/News.js
+++ src/lib/components/News/News.js
@@ -1,22 +1,42 @@
 import React, {useState} from "react";
-import {Avatar, Row} from "antd";
+import {Avatar, Modal, Row} from "antd";
 import {Ellipsis} from "ant-design-pro";
 
 export default function News({data}) {
 
+    const [modalVisibility, setModalVisibility] = useState(false);
+
     function listItemOnClick(e) {
         console.log("click", e);
+        setModalVisibility(true);
     }
 
     return (
-        <div onClick={listItemOnClick}>
-            <Row>
-                <Avatar shape="square" size="large" src={data.urlToImage}/>
-                {data.title}
-            </Row>
-            <Row>
-                <Ellipsis>{data.content}</Ellipsis>
-            </Row>
+        <div>
+            <div onClick={listItemOnClick}>
+                <Row>
+                    <Avatar shape="square" size="large" src={data.urlToImage}/>
+                    {data.title}
+                </Row>
+                <Row>
+                    <Ellipsis>{data.content}</Ellipsis>
+                </Row>
+            </div>
+            <Modal
+                visible={modalVisibility}
+                centered={true}
+                footer={null}
+                title={null}
+                maskClosable={true}
+                closable={false}
+                onCancel={() => {
+                    setModalVisibility(false)
+                }}
+            >
+                <p>{data.title}</p>
+                <p>{data.author}</p>
+                <p>{data.source.name}</p>
+            </Modal>
         </div>
     )
 }
