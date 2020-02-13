28944b115b808c5b39ba2f44b6cc6ae9751f8128
Bassam Salah Zakaria <fbsam.tkrcl@gmail.com>
2020-02-13 14:44:10 +0200

    correct word break

--- src/lib/components/Description/Description.js
+++ src/lib/components/Description/Description.js
@@ -1,12 +1,12 @@
 import {Ellipsis} from "ant-design-pro";
 import React from "react";
+import './Description.css'
 
 //200 because the APIs only get 200 chars and then add
 
 export function Description({description, content, length}) {
     return (<React.Fragment>
         {content &&
-        <Ellipsis style={{wordWrap: 'break-word'}} tooltip
-                  length={length ? length : 200}>{description ? description : content}</Ellipsis>}
+        <Ellipsis tooltip length={length ? length : 200}>{description ? description : content}</Ellipsis>}
     </React.Fragment>)
 }
